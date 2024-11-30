9525d2a2683cd5d9c1984cc97d318bf2ec50a40c
Kartik Shinde <142515173+karts43@users.noreply.github.com>
2024-11-30 11:18:09 +0530

    Update Remove duplicates in array.java

--- Remove duplicates in array.java
+++ Remove duplicates in array.java
@@ -3,8 +3,9 @@ class Solution {
     ArrayList<Integer> removeDuplicate(int arr[]) {
         ArrayList<Integer> answerstored = new ArrayList<>();
         for(int takevalues : arr){
-      if(!answerstored.contains(takevalues)) answerstored.add(takevalues);
+      if(!answerstored.contains(takevalues)){ answerstored.add(takevalues);
         }
+}
         return answerstored;
     }
 }
